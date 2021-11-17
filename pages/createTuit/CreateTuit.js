2ebd86b3836100aacf0ef1d7234d821097bd27cd
Dani Sanchez <danysq87@gmail.com>
2021-11-17 21:02:22 +0100

    Created Timeline.js and CreateTuit.js and their folders

---
+++ pages/createTuit/CreateTuit.js
@@ -0,0 +1,9 @@
+const CreateTuit = () => {
+  return (
+    <>
+      <h2>Create a new tuit</h2>
+    </>
+  );
+};
+
+export default CreateTuit;
---
+++ pages/timeline/Timeline.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const Timeline = ({ tuits }) => {
+  return (
+    <>
+      <h2>Timeline</h2>
+      <ul>
+        {tuits.map((tuit) => (
+          <li key={tuit.id}>
+            <Link
+              href={{
+                pathname: "twitList/[id]",
+                query: { id: tuit.id },
+              }}
+              passHref
+            >
+              <h3>{tuit.text}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Timeline;
+export const getServerSideProps = async () => {
+  const response = await fetch(
+    "https://isdi-blog-posts-api.herokuapp.com/posts"
+  );
+  const tuits = await response.json();
+
+  return {
+    props: { tuits },
+  };
+};
