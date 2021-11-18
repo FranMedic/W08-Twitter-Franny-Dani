d0dbcf5d71e6cb1c45334f07c7ab6d53952c7130
Franny Medico <m.francisca.medico@gmail.com>
2021-11-18 22:21:57 +0100

    added css to tuits, position to center, border, and display flex and backgorudnin tuits

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -2,38 +2,38 @@ import Link from "next/link";
 import TimeAgo from "javascript-time-ago";
 import es from "javascript-time-ago/locale/es.json";
 import ReactTimeAgo from "react-time-ago";
+import styles from "../../styles/Header.module.css";
 
 TimeAgo.addLocale(es);
 
 const Timeline = ({ tuits }) => {
   const onLike = async (id) => {
-    const response = await fetch(
-      `https://twitterapifrannydani.herokuapp.com/tuitah/${id}`,
-      {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+    await fetch(`https://twitterapifrannydani.herokuapp.com/tuitah/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+    });
   };
   return (
     <>
-      <h2>Timeline</h2>
-      <ul className="tuitlist">
-        {tuits
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
-          .map((tuit) => (
-            <li className="tuit" key={tuit.id}>
-              <Link href={`/timeline/${tuit.id}`}>
-                <a>{tuit.text}</a>
-              </Link>
-              <p>{tuit.likes}</p>
-              <ReactTimeAgo date={Date.parse(tuit.date)} locale="es" />
-              <button onClick={() => onLike(tuit.id)} className="like">
-                Like
-              </button>
-            </li>
-          ))}
-      </ul>
+      <div className={styles.tuitContainer}>
+        <h2 className={styles.tuitTitle}>Timeline</h2>
+        <ul className={styles.tuitlist}>
+          {tuits
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .map((tuit) => (
+              <li className={styles.tuit} key={tuit.id}>
+                <Link href={`/timeline/${tuit.id}`}>
+                  <a>{tuit.text}</a>
+                </Link>
+                <p>{tuit.likes}</p>
+                <ReactTimeAgo date={Date.parse(tuit.date)} locale="es" />
+                <button onClick={() => onLike(tuit.id)} className={styles.like}>
+                  Like
+                </button>
+              </li>
+            ))}
+        </ul>
+      </div>
     </>
   );
 };
