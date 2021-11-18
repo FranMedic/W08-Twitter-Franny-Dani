a460313925e1586c57b52ff0bd98aef65b2f5ec3
Dani Sanchez <danysq87@gmail.com>
2021-11-18 21:06:39 +0100

    Css header colors selection oh lah lah

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,10 +3,12 @@ import Link from "next/link";
 const Header = () => {
   return (
     <>
-      <h1>Tuiters</h1>
-      <Link href="/"> Home </Link>
-      <Link href="/createTuit"> Create Tuit </Link>
-      <Link href="/timeline"> Timeline </Link>
+      <div className="header">
+        <h1>Tuiters</h1>
+        <Link href="/"> Home </Link>
+        <Link href="/createTuit"> Create Tuit </Link>
+        <Link href="/timeline"> Timeline </Link>
+      </div>
     </>
   );
 };
