cdaa2d734a54d31ca0d2fbb7a1afb6c236d7c73b
Dani Sanchez <danysq87@gmail.com>
2021-11-18 21:21:01 +0100

    Bug fixed css module

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "../../styles/Header.module.css";
 
 const Header = () => {
   return (
     <>
-      <div className="header">
+      <div className={styles.header}>
         <h1>Tuiters</h1>
         <Link href="/"> Home </Link>
         <Link href="/createTuit"> Create Tuit </Link>
